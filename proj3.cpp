7d1b5dd065d1a1b3031b939dba14c7b709156ed4
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2024-11-11 00:32:40 -0500

    Revert "Changed TYPE_ATTR so that array types so that they print int[] instead of int in symbol table."
    This reverts commit 0d546d72d27eec1182726fc59a641de8e18c6c55.

--- proj3.cpp
+++ proj3.cpp
@@ -24,106 +24,66 @@ extern "C"
 #include "proj3.h"
 
 /*
- * symbol table entry
- */
-typedef struct
-{
-  std::unordered_map<int, uintptr_t> attrs; /* map from attribute names to values*/
-} SymbolTableEntry;
-
-/*
- * symbol table
- */
-static std::vector<SymbolTableEntry> symbolTable;
-
-/*
- * activation stack item
- */
-typedef struct
-{
-  int bottom; /* the bottom symbol table index for this scope */
-  int top;    /* the top symbol table index for this scope */
-} Scope;
-
-/*
- * activation stack where each stack item is a "scope" pointing to an offset in the symbol table where the scope begins
- */
-static std::deque<Scope> scopeStack;
-
-/*
- * current nesting level
- */
-static int nesting = 0;
-
-/*
- * whether to print out semantic errors
- */
-bool printSemanticErrors = false;
-
-/*
  * string table access functions defined in table.cpp.
  */
 int getIndex(const char *string);
 const char *getString(int i);
 
-/************************ routines *****************************/
+/// @brief Symbol table entry.
+typedef struct
+{
+  /// @brief List of attributes in (attr_num, attr_val) key value pairs.
+  std::unordered_map<int, uintptr_t> attrs;
+} SymbolTableEntry;
 
+/// @brief The symbol table.
+static std::vector<SymbolTableEntry> symbolTable;
+
+/// @brief Activation stack item.
+typedef struct
+{
+  /// @brief The bottom symbol table index for this scope.
+  int bottom;
+  /// @brief The top symbol table index for this scope.
+  int top;
+} Scope;
+
+/// @brief Activation stack where each stack item is a "scope" pointing to an offset in the symbol table where the scope begins.
+static std::deque<Scope> scopeStack;
+
+/// @brief Current nesting level in terms of scopes.
+static int nesting = 0;
+
+/// @brief Whether to print out semantic errors.
+bool printSemanticErrors = false;
+
+/// @brief Returns the number of entries in the symbol table.
+/// @return Number of entries in symbol table.
 int GetSymbolTableSize()
 {
   return symbolTable.size();
 }
 
-/*
- * STAddPredefined(): Add predefined names into symbol table.
- */
+/// @brief Adds predefined names into symbol table: "system", "readln", and "println", if they are used in the MINI-JAVA source code.
 void STAddPredefined()
 {
-  int nStrInd, nSymInd; /* string table index */
-
-  nStrInd = getIndex("system"); /* return string index of string "system" */
-  if (nStrInd != -1)            /* "system" is stored in string table */
-  {
-    nSymInd = InsertEntry(nStrInd, CLASS, 0);
-    SetAttr(nSymInd, PREDE_ATTR, true);
-  }
-
-  OpenBlock();
-
-  nStrInd = getIndex("readln");
-  if (nStrInd != -1)
-  {
-    nSymInd = InsertEntry(nStrInd, FUNC, 0);
-    SetAttr(nSymInd, PREDE_ATTR, true);
-    SetAttr(nSymInd, ARGNUM_ATTR, 1);
-  }
-
-  nStrInd = getIndex("println");
-  if (nStrInd != -1)
-  {
-    nSymInd = InsertEntry(nStrInd, FUNC, 0);
-    SetAttr(nSymInd, INIT_ATTR, 0);
-    SetAttr(nSymInd, PREDE_ATTR, true);
-    SetAttr(nSymInd, ARGNUM_ATTR, 1);
-  }
-
-  CloseBlock();
+  /* TODO: Implement function as described in above comment. */
 }
 
-/*
- * InsertEntry():  builds a symbol table entry for id.  the current block is
- * searched for redeclaration error.  the id's name  and nesting level
- * attributes are set by this function.  writing ErrorMessage routine is your
- * work.
- */
-int InsertEntry(int id, int kind, int lineno)
+/// @brief Adds a symbol table entry to the end of the symbol table.
+/// @param idx String table index of the name you are adding.
+/// @param kind Kind of symbol this is (VAR, FUNC, CLASS, ...).
+/// @param lineno Line number where the symbol is defined.
+/// @return The index of the new symbol table entry.
+int InsertEntry(int idx, int kind, int lineno)
 {
   /* id is already declared in the current block */
-  if (LookUpHere(id) != -1)
+  if (LookUpHere(idx) != -1)
   {
     return (-1);
   }
   SymbolTableEntry entry;
-  entry.attrs[NAME_ATTR] = id;
+  entry.attrs[NAME_ATTR] = idx;
   entry.attrs[KIND_ATTR] = kind;
   entry.attrs[LINENO_ATTR] = lineno;
   entry.attrs[NEST_ATTR] = nesting;
@@ -145,13 +105,10 @@ int InsertEntry(int id, int kind, int lineno)
   return symbolTable.size() - 1;
 }
 
-/*
- * LookUp(): search an id in the stack and return the symbol table entry
- * pointer, if found.  if the id is not in the stack, report error and push a
- * dummy item on the stack so that the same error will not be reported
- * repeatedly
- */
-int LookUp(int id)
+/// @brief Looks up a name in the symbol table in all active scopes, starting from innermost.
+/// @param idx String table index of the name you are looking for.
+/// @return The index of the found symbol table entry (-1 if not found).
+int LookUp(int idx)
 {
   for (auto it = scopeStack.rbegin(); it != scopeStack.rend(); ++it)
   {
@@ -159,19 +116,16 @@ int LookUp(int id)
     if (it->bottom == -1)
       continue;
     for (int idx = it->bottom; idx <= it->top; idx++)
-      if (GetAttr(idx, NAME_ATTR) == id)
+      if (GetAttr(idx, NAME_ATTR) == idx)
         return idx;
   }
   return -1;
 }
 
-/*
- * LookUpHere():  search an id in the stack for the current block.  It
- * returns the symbol table pointer if the id is found.  otherwise, return 0
- * this routine can be used to check if there is a forward declaration
- * for a procedure/function
- */
-int LookUpHere(int id)
+/// @brief Looks up a name in the symbol table only in the current scope (block).
+/// @param idx String table index of the name you are looking for.
+/// @return The index of the found symbol table entry (-1 if not found).
+int LookUpHere(int idx)
 {
   // Check if the current scope is empty.
   if (scopeStack.empty() || scopeStack.back().bottom == -1)
@@ -179,24 +133,14 @@ int LookUpHere(int id)
 
   for (int idx = scopeStack.back().bottom; idx <= scopeStack.back().top; idx++)
   {
-    if (GetAttr(idx, NAME_ATTR) == id)
+    if (GetAttr(idx, NAME_ATTR) == idx)
       return idx;
   }
 
   return -1;
 }
 
-/*
- * LookUpField():  it is found unnecessary.  so it is not implemented
- */
-
-/*
- * OpenBlock():  build a mark on the stack to indecate the beginning of a new
- * block. increment nesting level counter.  It is called when reserved words
- * "program", "procedure", "function" or "record" is encountered.  Note:
- * procedure or function name should be inserted into symbol table before
- * calling this routine.
- */
+/// @brief Push a new scope into the scope stack and increment nesting level.
 void OpenBlock()
 {
   Scope scope = {-1, -1};
@@ -204,114 +148,88 @@ void OpenBlock()
   nesting++;
 }
 
-/*
- * CloseBlock():  decrement nesting level counter and remove the current
- * block from the stack.  called when exitting from a program, procedure,
- * function or a record definition. each element is checked to see if it is
- * used in the block
- */
+/// @brief Pops this scope from the scope stack and decrement nesting level.
 void CloseBlock()
 {
   scopeStack.pop_back();
   nesting--;
 }
 
-/*
- * IsAttr(): return the index to the searched attribute in attrarray if it is
- * in (nonzero).  otherwise, return false.
- */
-int IsAttr(int st_ptr, int attr_num)
+/// @brief Checks whether the given attribute number exists for the given symbol.
+/// @param st_idx Symbol table index for the queried symbol.
+/// @param attr_num The target attribute number.
+/// @return 1 if attribute exists, 0 if it does not exist.
+int IsAttr(int st_idx, int attr_num)
 {
-  if (st_ptr >= symbolTable.size())
+  if (st_idx >= symbolTable.size())
   {
-    printf("DEBUG--Symbol table entry %d does not exist.\n", st_ptr);
+    printf("DEBUG--Symbol table entry %d does not exist.\n", st_idx);
     assert(0);
   }
-  if (symbolTable[st_ptr].attrs.find(attr_num) != symbolTable[st_ptr].attrs.end())
+  if (symbolTable[st_idx].attrs.find(attr_num) != symbolTable[st_idx].attrs.end())
     return 1;
   else
     return 0;
 }
 
-/*
- * GetAttr(): return the specified attribute value for a symbol table entry
- * if found.  otherwise, report error.  Note, this error is not the fault of
- * source program but of the compiler writer.  It is printed for testing and
- * debugging.
- */
-uintptr_t GetAttr(int st_ptr, int attr_num)
+/// @brief Returns the value of the given attribute number for the given symbol.
+/// @param st_idx Symbol table index for the queried symbol.
+/// @param attr_num The target attribute number.
+/// @return Value of the attribute (if not found, prints a debug error message and returns 0).
+uintptr_t GetAttr(int st_idx, int attr_num)
 {
-  if (st_ptr >= symbolTable.size())
+  if (st_idx >= symbolTable.size())
   {
-    printf("DEBUG--Symbol table entry %d does not exist.\n", st_ptr);
+    printf("DEBUG--Symbol table entry %d does not exist.\n", st_idx);
     assert(0);
   }
-  if (symbolTable[st_ptr].attrs.find(attr_num) == symbolTable[st_ptr].attrs.end())
+  if (symbolTable[st_idx].attrs.find(attr_num) == symbolTable[st_idx].attrs.end())
   {
     printf("DEBUG--The wanted attribute number %d does not exist\n", attr_num);
     return 0;
   }
-  return symbolTable[st_ptr].attrs[attr_num];
+  return symbolTable[st_idx].attrs[attr_num];
 }
 
-/*
- * SetAttr(): set attribute.  if the attribute is already there,  print
- * debugging message. attributes for a symbol table entry are sorted by their
- * attr_num.
- */
-void SetAttr(int st_ptr, int attr_num, uintptr_t attr_val)
+/// @brief Sets the value of the given attribute number for the given symbol.
+/// @param st_idx Symbol table index for the target symbol.
+/// @param attr_num The target attribute number.
+void SetAttr(int st_idx, int attr_num, uintptr_t attr_val)
 {
-  if (st_ptr >= symbolTable.size())
+  if (st_idx >= symbolTable.size())
   {
-    printf("DEBUG--Symbol table entry %d does not exist.\n", st_ptr);
+    printf("DEBUG--Symbol table entry %d does not exist.\n", st_idx);
     assert(0);
   }
-  if (symbolTable[st_ptr].attrs.find(attr_num) != symbolTable[st_ptr].attrs.end() && attr_num != IS_USED_ATTR)
+  if (symbolTable[st_idx].attrs.find(attr_num) != symbolTable[st_idx].attrs.end() && attr_num != IS_USED_ATTR)
   {
     printf("DEBUG--The attribute number %d to be added already exists\n", attr_num);
     return;
   }
-  symbolTable[st_ptr].attrs[attr_num] = attr_val;
+  symbolTable[st_idx].attrs[attr_num] = attr_val;
 }
 
 const char *kind_name[] = {"variable", "function", "ref_arg", "val_arg", "class", "program", "array"};
 
-std::string getNodeString(tree nd)
+const char *getNodeString(tree nd)
 {
   switch (NodeKind(nd))
   {
   case EXPRNode:
     return getNodeOpString(nd);
-  case NUMNode:
-    return "NUMNode(" + std::to_string(IntVal(nd)) + ")";
-  default:
-    printf("DEBUG--Unprintable node kind %s\n", kind_name[NodeKind(nd)]);
-    assert(0);
-  }
-}
-
-std::string getTypeString(tree nd)
-{
-  switch (NodeKind(nd))
-  {
   case INTEGERTNode:
     return "int";
-  case EXPRNode:
+  case STNode:
   {
-    assert(NodeOp(nd) == TypeIdOp);
-    std::string type;
-    if (NodeKind(LeftChild(nd)) == INTEGERTNode)
-      type = "int";
-    else
-    {
-      assert(NodeKind(LeftChild(nd)) == STNode);
-      int idx = IntVal(LeftChild(nd));
-      assert(idx > 0);
-      type = getString(GetAttr(idx, NAME_ATTR));
-    }
-    for (tree indexOp = RightChild(nd); NodeOp(indexOp) == IndexOp; indexOp = RightChild(indexOp))
-      type += "[]";
-    return type;
+    int idx = IntVal(nd);
+    assert(idx > 0);
+    return getString(GetAttr(idx, NAME_ATTR));
+  }
+  case NUMNode:
+  {
+    static char buf[32];
+    snprintf(buf, 32, "NUMNode(%d)", IntVal(nd));
+    return buf;
   }
   case DUMMYNode:
     return "void";
@@ -335,10 +253,9 @@ const char *getDimensionsString(std::vector<int> *dims)
   return buf;
 }
 
-/*
- * STPrint():  print symbol table.  This routine is incomplete, please fill
- * in details.
- */
+/// @brief Prints out all the symbols in the symbol table.
+/// @param table The FILE pointer where the printing should be directed.
+/// @param printLineNo Whether to print out the line number attribute of each symbol.
 void STPrint(FILE *table, bool printLineNo)
 {
   int i, attr_num, attr_ptr, type_attr;
@@ -372,14 +289,14 @@ void STPrint(FILE *table, bool printLineNo)
           break;
         case INIT_ATTR:
         {
-          std::string node_str = attr_val ? getNodeString((tree)attr_val) : "";
-          fprintf(table, "%14s", node_str.c_str());
+          const char *node_str = attr_val ? getNodeString((tree)attr_val) : "";
+          fprintf(table, "%14s", node_str);
         }
         break;
         case TYPE_ATTR:
         {
-          std::string type_str = attr_val ? getTypeString((tree)attr_val) : "";
-          fprintf(table, "%11s", type_str.c_str());
+          const char *node_str = attr_val ? getNodeString((tree)attr_val) : "";
+          fprintf(table, "%11s", node_str);
         }
         break;
         case PREDE_ATTR:
@@ -430,7 +347,11 @@ void STPrint(FILE *table, bool printLineNo)
   }
 }
 
-void ErrorMessage(int type, int lineno, int id, int seq)
+/// @brief Prints out a semantic error message.
+/// @param type The type of error.
+/// @param lineno The line number where the error occurred.
+/// @param idx The string table index of the relevant symbol with the error.
+void ErrorMessage(int type, int lineno, int idx)
 {
   if (!printSemanticErrors)
     return;
@@ -439,40 +360,40 @@ void ErrorMessage(int type, int lineno, int id, int seq)
   switch (type)
   {
   case REDECLARATION:
-    printf("symbol %s is previously redeclared.\n", getString(id));
+    printf("symbol %s is previously redeclared.\n", getString(idx));
     break;
   case UNDECLARED:
-    printf("symbol %s is undeclared.\n", getString(id));
+    printf("symbol %s is undeclared.\n", getString(idx));
     break;
   case NOT_USED:
-    printf("variable %s is not used anywhere.\n", getString(id));
+    printf("variable %s is not used anywhere.\n", getString(idx));
     break;
   case TYPE_MISMATCH:
-    printf("symbol %s the type does not allow this operation.\n", getString(id));
+    printf("symbol %s the type does not allow this operation.\n", getString(idx));
     break;
   case ARGUMENTS_NUM:
-    printf("function %s argument number is different from declared parameter number.\n", getString(id));
+    printf("function %s argument number is different from declared parameter number.\n", getString(idx));
     break;
   case DIMENSIONS_NUM:
-    printf("array %s accessed with an inappropriate number of dimensions.\n", getString(id));
+    printf("array %s accessed with an inappropriate number of dimensions.\n", getString(idx));
     break;
   case BOUND_NOT_CONST:
-    printf("symbol %s is not a constant so can't be used as an array bound.\n", getString(id));
+    printf("symbol %s is not a constant so can't be used as an array bound.\n", getString(idx));
     break;
   case NOT_A_FIELD:
-    printf("symbol %s is not a field in the class.\n", getString(id));
+    printf("symbol %s is not a field in the class.\n", getString(idx));
     break;
   case NOT_AN_ARRAY:
-    printf("symbol %s not an array so can't be indexed.\n", getString(id));
+    printf("symbol %s not an array so can't be indexed.\n", getString(idx));
     break;
   case NOT_A_VARIABLE:
-    printf("symbol %s not a variable so has no value.\n", getString(id));
+    printf("symbol %s not a variable so has no value.\n", getString(idx));
     break;
   case NOT_A_FUNCTION:
-    printf("symbol %s not a function so can't be called.\n", getString(id));
+    printf("symbol %s not a function so can't be called.\n", getString(idx));
     break;
   case NOT_A_TYPE:
-    printf("symbol %s is not a type.\n", getString(id));
+    printf("symbol %s is not a type.\n", getString(idx));
     break;
   default:
     printf("error type: %d.\n", type);
